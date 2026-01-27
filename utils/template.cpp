5ab2396a86e1f94f1a92d3f5dc2983275e342b0d
Jeank-J <jeankjimenezortega@gmail.com>
2026-01-27 10:59:26 -0500

    Template upd

--- utils/template.cpp
+++ utils/template.cpp
@@ -1,6 +1,17 @@
+//g++ -fsanitize=address,undefined -fno-omit-frame-pointer -g -Wall -Wshadow -std=c++17 -Wno-unused-result -Wno-sign-compare -Wno-char-subscripts -DLOCAL -o code1 code1.cpp && ./code1 < in.txt
 #include <bits/stdc++.h>
 
 using namespace std;
+
+#define dbg(...) cerr << #__VA_ARGS__ << " = ["; dbg_out(__VA_ARGS__); cerr << "]\n"
+void dbg_out() {}
+
+template <typename T, typename... Args>
+void dbg_out(T x, Args... args) {
+    cerr << x;
+    if (sizeof...(args)) cerr << ",";
+    dbg_out(args...);
+}
 
 #define forn(i,n) for (int i = 0; i < n; i++)
 #define rforn(i,n) for (int i = n-1; i >= 0; i--)
@@ -8,30 +19,30 @@ using namespace std;
 #define ff first
 #define ss second
 #define pb push_back
-#define ll long long
-#define all(x) x.begin(),x.end()
+#define ll long long 
 #define sz(n) (int)n.size()
+#define all(x) x.begin(), x.end()
 #define read(x) for(auto &el : x) cin >> el;
 #define show(x) for (auto &i: x) cout << i << " "; cout << endl;
 #define FAST ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 
-
-const int MOD = 1e9 + 7;
-const int N = 2e5 + 7;
-const int INF = 1e9;
-
-
+#define pc(x) __builtin_popcount(x)
+#define lsb(x) ((x) & -(x))
+#define lsb_idx(x) __builtin_ctz(x)
+#define on(x,i) ((x) |= (1LL << (i)))
+#define off(x,i) ((x) &= ~(1LL << (i)))
+#define toggle(x,i) ((x) ^= (1LL << (i)))
 
 void solve(){
+    
 
-    // code here
 
 }
 
 int main() {
 
-    FAST    
-    int test = 1;    
+    FAST        
+    int test = 1;        
     cin >> test;
     while (test--) solve();
 
